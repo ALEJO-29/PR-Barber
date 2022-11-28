309e4d246b8f665242d401956e3e6a564273e336
ALEJO-29 <alejandro123aut@gmail.com>
2022-11-28 16:57:57 -0500

    vista publicidad

--- barberiaHarri/barberApp/admin.py
+++ barberiaHarri/barberApp/admin.py
@@ -1,6 +1,6 @@
 import site
 from django.contrib import admin
-from .models import Servicio, Producto, TipoProducto, Cita, Usuario
+from .models import  Producto, TipoProducto, Cita, Usuario
 
 # Register your models here.
 
@@ -8,7 +8,6 @@ admin.site.site_header = 'Barber Harry'
 admin.site.index_title = 'Panel de control Barber Harry'
 admin.site.site_title = 'Administrador de la pagina'
 
-admin.site.register(Servicio)
 admin.site.register(Producto)
 admin.site.register(TipoProducto)
 admin.site.register(Cita)
--- barberiaHarri/barberApp/models.py
+++ barberiaHarri/barberApp/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from datetime import date, timedelta
 # Create your models here.
 
- 
+
 day = date.today()
+
 
 class Producto(models.Model):
     imagen = models.ImageField(upload_to="images/", blank=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=150)
     precio = models.IntegerField()
-    cantidad= models.IntegerField(default=True)
-    
+    cantidad = models.IntegerField(default=True)
+
     def __str__(self):
         return self. nombre
 
@@ -19,7 +20,7 @@ class Producto(models.Model):
 class TipoProducto(models.Model):
     descripcion = models.CharField(max_length=150)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.producto
 
@@ -29,25 +30,13 @@ class Usuario(models.Model):
     apellido = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
-    
+
     def __str__(self):
         return self.nombre
 
-    
-
-class Servicio(models.Model):
-    imagen = models.ImageField(upload_to="images/", blank=True)
-    tipo_servicio = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=150)
-    precio = models.IntegerField()
-    #usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.tipo_servicio
-
 
 class Cita(models.Model):
-    tipo_corte=(
+    tipo_corte = (
         ('corte', 'corte'),
         ('cejas', 'cejas'),
         ('barba', 'barba'),
@@ -55,9 +44,15 @@ class Cita(models.Model):
         ('corte y barba', 'corte y barba'),
         ('corte completo', 'corte completo'),
     )
-    
+
     fecha = models.DateField(verbose_name="fecha", blank=True, null=True)
-    hora = models.TimeField(verbose_name="hora", blank=True,unique=True)
+    hora = models.TimeField(verbose_name="hora", blank=True, unique=True)
     descripcion = models.CharField(max_length=50)
-    tipo_corte= models.CharField(max_length=250, choices=tipo_corte, default=True)
-    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    tipo_corte = models.CharField(
+        max_length=250, choices=tipo_corte, default=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+
+
+    
+    
